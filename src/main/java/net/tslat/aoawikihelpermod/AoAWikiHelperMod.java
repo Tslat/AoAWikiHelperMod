b2d040a004fce5acd4f414b1fa9604c45355cb94
Tslat <tslatmail@gmail.com>
2022-04-22 14:30:08 +1000

    Update version to 2.9.2

--- src/main/java/net/tslat/aoawikihelpermod/AoAWikiHelperMod.java
+++ src/main/java/net/tslat/aoawikihelpermod/AoAWikiHelperMod.java
@@ -29,7 +29,7 @@ import static net.tslat.aoawikihelpermod.AoAWikiHelperMod.MOD_ID;
 
 @Mod(MOD_ID)
 public class AoAWikiHelperMod {
-	public static final String VERSION = "2.9.1";
+	public static final String VERSION = "2.9.2";
 	public static final String MOD_ID = "aoawikihelpermod";
 	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
