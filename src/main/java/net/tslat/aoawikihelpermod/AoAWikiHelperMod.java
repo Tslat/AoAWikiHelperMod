c307df6862d4457f8c69c9f883c63937d9b2d8e0
Tslat <tslatmail@gmail.com>
2022-05-15 10:23:58 +1000

    Update to 2.7.3 Should make the mod work again

--- src/main/java/net/tslat/aoawikihelpermod/AoAWikiHelperMod.java
+++ src/main/java/net/tslat/aoawikihelpermod/AoAWikiHelperMod.java
@@ -29,7 +29,7 @@ import static net.tslat.aoawikihelpermod.AoAWikiHelperMod.MOD_ID;
 
 @Mod(MOD_ID)
 public class AoAWikiHelperMod {
-	public static final String VERSION = "2.7.2";
+	public static final String VERSION = "2.7.3";
 	public static final String MOD_ID = "aoawikihelpermod";
 	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
