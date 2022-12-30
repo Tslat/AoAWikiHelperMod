3c65ac3ee8eec48c005a3b40093d284ee44bb50a
c64cosmin <c64cosmin@gmail.com>
2022-12-30 03:10:42 +0200

    Update version to 2.9.17

--- src/main/java/net/tslat/aoawikihelpermod/AoAWikiHelperMod.java
+++ src/main/java/net/tslat/aoawikihelpermod/AoAWikiHelperMod.java
@@ -38,7 +38,7 @@ import static net.tslat.aoawikihelpermod.AoAWikiHelperMod.MOD_ID;
 
 @Mod(MOD_ID)
 public class AoAWikiHelperMod {
-	public static final String VERSION = "2.9.16";
+	public static final String VERSION = "2.9.17";
 	public static final String MOD_ID = "aoawikihelpermod";
 	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
