eaee4ba21270ccd25c7aaab1a23a36b2ee5f8111
Tslat <tslatmail@gmail.com>
2024-08-02 21:42:54 +1000

    Update to support NumberProvider

--- src/main/java/net/tslat/aoawikihelpermod/util/loottable/function/GrantSkillXpFunctionHelper.java
+++ src/main/java/net/tslat/aoawikihelpermod/util/loottable/function/GrantSkillXpFunctionHelper.java
@@ -1,7 +1,6 @@
 package net.tslat.aoawikihelpermod.util.loottable.function;
 
 import net.tslat.aoa3.content.loottable.function.GrantSkillXp;
-import net.tslat.aoa3.util.NumberUtil;
 import net.tslat.aoawikihelpermod.util.FormattingHelper;
 
 import javax.annotation.Nonnull;
@@ -10,6 +9,6 @@ public class GrantSkillXpFunctionHelper extends LootFunctionHelper<GrantSkillXp>
 	@Nonnull
 	@Override
 	public String getDescription(GrantSkillXp function) {
-		return "will additionally grant " + NumberUtil.roundToNthDecimalPlace(function.getXp(), 2) + " " + FormattingHelper.createLinkableText(function.getSkill().getName().getString(), false, true) + " xp";
+		return "will additionally grant " + FormattingHelper.getStringFromRange(function.getXp()) + " " + FormattingHelper.createLinkableText(function.getSkill().getName().getString(), false, true) + " xp";
 	}
 }
